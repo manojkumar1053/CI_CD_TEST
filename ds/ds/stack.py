5900a2ef4fd6f4454d44f8c62d4ce7c957f01810
manojkumar1053 <manojkumar1053@gmail.com>
2021-12-06 22:19:28 -0500

    stakc test

--- ds/ds/stack.py
+++ ds/ds/stack.py
@@ -1,10 +1,13 @@
 class Stack:
-    
+
     def __init_(self):
         self._storage = []
 
     def __len__(self):
         return len(self._storage)
-    
+
     def push(self, item):
         self._storage.append(item)
+
+    def pop(self):
+        return self._storage.pop()
--- ds/tests/test_stack.py
+++ ds/tests/test_stack.py
@@ -1,15 +1,28 @@
 from ds.stack import Stack
 import pytest
 
+
 @pytest.fixture
 def stack():
     return Stack()
+
 
 def test_constructor():
     s = Stack()
     assert isinstance(s, Stack)
     assert len(s) == 0
 
-def test_push(stack): 
+
+def test_push(stack):
     stack.push(3)
     assert len(stack) == 1
+    stack.push(5)
+    assert len(stack) == 2
+
+
+def test_pop(self):
+    stack.push("ons")
+    stack.push("world")
+    assert stack.pop() == "world"
+    assert stack.pop() == "ons"
+    assert stack.pop() is None
